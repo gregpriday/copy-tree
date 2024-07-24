015a6cccaf6c6591e7af6add2f71450359ff60ca
Greg Priday <greg@siteorigin.com>
2024-07-24 14:28:27 +0200

    Fix iterator

--- src/Ruleset/FilteredDirIterator.php
+++ src/Ruleset/FilteredDirIterator.php
@@ -19,32 +19,67 @@ class FilteredDirIterator extends \RecursiveDirectoryIterator
         '.yarn',
     ];
 
+    private $isSkippedDir = false;
+
+    public function __construct($directory, $flags = \FilesystemIterator::KEY_AS_PATHNAME | \FilesystemIterator::CURRENT_AS_FILEINFO)
+    {
+        parent::__construct($directory, $flags);
+        $this->skipUnwantedFiles();
+    }
+
+    public function hasChildren($allowLinks = false): bool
+    {
+        return !$this->isSkippedDir && parent::hasChildren($allowLinks) && !$this->shouldSkip($this->getPathname());
+    }
+
     public function getChildren(): \RecursiveDirectoryIterator
     {
-        try {
-            $children = parent::getChildren();
-            if ($children->isDir() && $this->shouldSkip($children->getPathname())) {
-                $children->next();
-            }
-
-            return $children;
-        } catch (\UnexpectedValueException $e) {
-            return new self($this->getPath(), $this->getFlags());
+        if ($this->hasChildren()) {
+            return new self($this->getPathname(), $this->getFlags());
         }
+        $this->isSkippedDir = true;
+        return $this;
     }
 
     public function next(): void
     {
-        parent::next();
-        while ($this->valid() && $this->shouldSkip($this->getPathname())) {
+        do {
             parent::next();
-        }
+            $this->skipUnwantedFiles();
+        } while ($this->valid() && $this->shouldSkip($this->getPathname()));
+    }
+
+    public function valid(): bool
+    {
+        return !$this->isSkippedDir && parent::valid() && !$this->shouldSkip($this->getPathname());
+    }
+
+    public function rewind(): void
+    {
+        parent::rewind();
+        $this->skipUnwantedFiles();
+    }
+
+    public function current(): \SplFileInfo
+    {
+        return parent::current();
+    }
+
+    public function key(): string
+    {
+        return parent::key();
     }
 
     private function shouldSkip(string $path): bool
     {
         $basename = basename($path);
-
         return in_array($basename, self::$ignoredDirs) || str_starts_with($basename, '.');
+    }
+
+    private function skipUnwantedFiles(): void
+    {
+        while (parent::valid() && $this->shouldSkip($this->getPathname())) {
+            parent::next();
+        }
     }
 }
