6fc71f409601e9aa0df1a22d2eb10dbb5f8d49f5
gregpriday <greg@siteorigin.com>
2024-11-04 11:08:39 +0200

    Test updates

--- tests/CopyTreeCommandTest.php
+++ tests/CopyTreeCommandTest.php
@@ -12,7 +12,13 @@ class CopyTreeCommandTest extends TestCase
 
         $output = $this->commandTester->getDisplay();
         $this->assertStringContainsString('Copied', $output);
-        $this->assertFilesCopied($output, 15); // Adjust this number as needed
+
+        // Make sure some files were copied, without specifying an exact number
+        if (preg_match('/Copied (\d+) files to clipboard/', $output, $matches)) {
+            $this->assertGreaterThan(0, (int) $matches[1], "Expected some files to be copied");
+        } else {
+            $this->fail("Could not find the number of files copied in the output: $output");
+        }
     }
 
     public function testExecuteWithFilter()
@@ -24,7 +30,22 @@ class CopyTreeCommandTest extends TestCase
 
         $output = $this->commandTester->getDisplay();
         $this->assertStringContainsString('Copied', $output);
-        $this->assertFilesCopied($output, 1);
+        $this->assertFilesCopied($output, 2); // Will match test1.txt and test2.txt
+    }
+
+    public function testExecuteWithMultipleFilters()
+    {
+        $this->commandTester->execute([
+            'path' => __DIR__.'/dir',
+            '--filter' => [
+                '*/*.txt',
+                '*/*.md'
+            ],
+        ]);
+
+        $output = $this->commandTester->getDisplay();
+        $this->assertStringContainsString('Copied', $output);
+        $this->assertFilesCopied($output, 4); // Will match two .txt files and two .md files
     }
 
     public function testExecuteWithLaravelProject()
@@ -37,7 +58,15 @@ class CopyTreeCommandTest extends TestCase
 
         $output = $this->commandTester->getDisplay();
         $this->assertStringContainsString('Copied', $output);
-        $this->assertFilesCopied($output, 15);
+
+        // Make sure a reasonable number of Laravel files were copied
+        if (preg_match('/Copied (\d+) files to clipboard/', $output, $matches)) {
+            $numFiles = (int) $matches[1];
+            $this->assertGreaterThan(10, $numFiles, "Expected a substantial number of Laravel files to be copied");
+            $this->assertLessThan(50, $numFiles, "Unexpectedly large number of Laravel files copied");
+        } else {
+            $this->fail("Could not find the number of files copied in the output: $output");
+        }
     }
 
     public function testExecuteWithDisplayOption()
@@ -63,7 +92,14 @@ class CopyTreeCommandTest extends TestCase
 
         $output = $this->commandTester->getDisplay();
         $this->assertStringContainsString('Saved', $output);
-        $this->assertFilesCopied($output, 15);
+
+        // Make sure some files were saved, without specifying an exact number
+        if (preg_match('/Saved (\d+) files to/', $output, $matches)) {
+            $this->assertGreaterThan(0, (int) $matches[1], "Expected some files to be saved");
+        } else {
+            $this->fail("Could not find the number of files saved in the output: $output");
+        }
+
         $this->assertFileExists($outputFile);
         $fileContents = file_get_contents($outputFile);
         $this->assertStringContainsString('.', $fileContents); // Root directory
@@ -93,6 +129,9 @@ class CopyTreeCommandTest extends TestCase
         // Create a temporary custom ruleset file in the project root
         $projectRoot = dirname(__DIR__); // Go up one level from the tests directory
         $rulesetDir = $projectRoot.'/.ctree';
+        if (!is_dir($rulesetDir)) {
+            mkdir($rulesetDir);
+        }
         $rulesetFile = $rulesetDir.'/test-ruleset.json';
 
         $rulesetContent = json_encode([
@@ -119,5 +158,6 @@ class CopyTreeCommandTest extends TestCase
 
         // Clean up
         unlink($rulesetFile);
+        rmdir($rulesetDir);
     }
 }
